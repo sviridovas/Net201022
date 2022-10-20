3d491828fb87488277557dc86f9200204cb77ed1
utesurdako <arktnels@gmail.com>
2022-10-20 22:17:36 +0300

    init commit

---
+++ Assets/ProjectData/Scripts/Authorization.cs
@@ -0,0 +1,69 @@
+using Photon.Pun;
+using Photon.Realtime;
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Authorization : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string _playFabTitle;
+
+    void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+            PlayFabSettings.staticSettings.TitleId = _playFabTitle;
+
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = "TestUser",
+            CreateAccount = true
+        };
+
+        PlayFabClientAPI.LoginWithCustomID(request,
+            result =>
+            {
+                Debug.Log(result.PlayFabId);
+                PhotonNetwork.AuthValues = new AuthenticationValues(result.PlayFabId);
+                PhotonNetwork.NickName = result.PlayFabId;
+                Connect();
+            },
+            error => Debug.LogError(error));
+    }
+
+    private void Connect()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomOrCreateRoom(roomName: $"Room N{Random.Range(0, 9999)}");
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = PhotonNetwork.AppVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("OnConnectedToMaster");
+        if (!PhotonNetwork.InRoom)
+            PhotonNetwork.JoinRandomOrCreateRoom(roomName: $"Room N{Random.Range(0, 9999)}");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("OnCreatedRoom");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log($"OnJoinedRoom {PhotonNetwork.CurrentRoom.Name}");
+    }
+}
