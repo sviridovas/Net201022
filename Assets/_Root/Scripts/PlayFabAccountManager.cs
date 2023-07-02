ed810d7da82a5cf00b7073931e0f7fcb7478d947
admin <alexander.sviridof@gmail.com>
2023-07-02 18:32:38 +0300

    ДЗ 4

---
+++ Assets/_Root/Scripts/PlayFabAccountManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class PlayFabAccountManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _titleLabel;
+    private void Start()
+    {
+        PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(),
+        OnGetAccountSuccess, OnFailure);
+    }
+    private void OnGetAccountSuccess(GetAccountInfoResult result)
+    {
+        _titleLabel.text = $"Welcome back, Player {result.AccountInfo.TitleInfo.DisplayName} with ID {result.AccountInfo.PlayFabId}, ";
+    }
+    private void OnFailure(PlayFabError error)
+    {
+        var errorMessage = error.GenerateErrorReport();
+        Debug.LogError($"Something went wrong: {errorMessage}");
+    }
+}
--- Assets/_Root/Scripts/PlayFabLogin.cs
+++ Assets/_Root/Scripts/PlayFabLogin.cs
@@ -3,20 +3,41 @@ using PlayFab.ClientModels;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class PlayFabLogin : MonoBehaviour
 {
-    [SerializeField] Button loginBtn = null;
+    [SerializeField] TMP_InputField username;
+    [SerializeField] TMP_InputField email;
+    [SerializeField] TMP_InputField password;
+    [SerializeField] Button signUp;
+    [SerializeField] Button loginBtn;
+    [SerializeField] TMP_Text error;
+
+    private const string AuthGuidKey = "authorization-guid";
 
     public void Start()
     {
-        if(loginBtn != null) {
-            loginBtn.onClick = new Button.ButtonClickedEvent();
-            loginBtn.onClick.AddListener(LogInOut);
+        // error.enabled = false;
+        error.text = "Loadiing...";
+
+        if (signUp != null)
+        {
+            signUp.onClick = new Button.ButtonClickedEvent();
+            signUp.onClick.AddListener(CreateAccount);
         }
+
+        if (loginBtn != null)
+        {
+            loginBtn.onClick = new Button.ButtonClickedEvent();
+            loginBtn.onClick.AddListener(SignIn);
+        }
+
+        LoginWithCustomID();
     }
 
-    public void LogInOut(){
+    public void LoginWithCustomID()
+    {
         // Here we need to check whether TitleId property is configured in settings or not
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
         {
@@ -24,34 +45,92 @@ public class PlayFabLogin : MonoBehaviour
             * If not we need to assign it to the appropriate variable manually
             * Otherwise we can just remove this if statement at all
             */
-            PlayFabSettings.staticSettings.TitleId = "20С7А";
+            PlayFabSettings.staticSettings.TitleId = "20C7F";
         }
-     
-        if(!PlayFabClientAPI.IsClientLoggedIn()) {
-            var request = new LoginWithCustomIDRequest { CustomId = "GeekBrainsLesson3", CreateAccount = true };
-            PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-        } else {
-            PlayFabClientAPI.ForgetAllCredentials();
-            
-            loginBtn.image.color = Color.blue;
-            loginBtn.GetComponentInChildren<TMP_Text>().text = "Log In";
-        }
+
+        var needCreation = PlayerPrefs.HasKey(AuthGuidKey);
+        var id = PlayerPrefs.GetString(AuthGuidKey, Guid.NewGuid().ToString());
+
+        PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest()
+        {
+            CustomId = id,
+            CreateAccount = !needCreation
+        }, success => { 
+            error.enabled = false;
+            PlayerPrefs.SetString(AuthGuidKey, id); 
+        }, err =>
+        {
+            Debug.LogError($"Fail: {err.ErrorMessage}");
+            error.enabled = true;
+            error.text = err.ErrorMessage;
+        });
     }
 
-    public void OnLoginSuccess(LoginResult result)
+    // public void LogOut()
+    // {
+    //     PlayFabClientAPI.ForgetAllCredentials();
+
+    //     loginBtn.image.color = Color.blue;
+    //     loginBtn.GetComponentInChildren<TMP_Text>().text = "Log In";
+    // }
+
+    // public void OnLoginSuccess(LoginResult result)
+    // {
+    //     Debug.Log("Congratulations, you made successful API call!");
+
+    //     loginBtn.image.color = Color.green;
+    //     loginBtn.GetComponentInChildren<TMP_Text>().text = "Log Out";
+    // }
+
+    // private void OnLoginFailure(PlayFabError error)
+    // {
+    //     var errorMessage = error.GenerateErrorReport();
+    //     Debug.LogError($"Something went wrong: {errorMessage}");
+
+    //     loginBtn.image.color = Color.blue;
+    //     loginBtn.GetComponentInChildren<TMP_Text>().text = "Log In";
+    // }
+
+    public void CreateAccount()
     {
-        Debug.Log("Congratulations, you made successful API call!");
+        var userName = username.text;
 
-        loginBtn.image.color = Color.green;
-        loginBtn.GetComponentInChildren<TMP_Text>().text = "Log Out";
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        {
+            Username = userName,
+            Email = email.text,
+            Password = password.text,
+            RequireBothUsernameAndEmail = true
+        }, result =>
+        {
+            Debug.Log($"Success: {userName}");
+            error.enabled = false;
+        }, err =>
+        {
+            Debug.LogError($"Fail: {err.ErrorMessage}");
+            error.enabled = true;
+            error.text = err.ErrorMessage;
+        });
     }
 
-    private void OnLoginFailure(PlayFabError error)
+    public void SignIn()
     {
-        var errorMessage = error.GenerateErrorReport();
-        Debug.LogError($"Something went wrong: {errorMessage}");
-        
-        loginBtn.image.color = Color.blue;
-        loginBtn.GetComponentInChildren<TMP_Text>().text = "Log In";
+        var userName = username.text;
+
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+        {
+            Username = userName,
+            Password = password.text
+        }, result =>
+        {
+            Debug.Log($"Success: {userName}");
+            error.enabled = false;
+        }, err =>
+        {
+            Debug.LogError($"Fail: {err.ErrorMessage}");
+            error.enabled = true;
+            error.text = err.ErrorMessage;
+        });
     }
+
 }
